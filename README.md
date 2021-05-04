baecc5c8042eeec33cddfa55faf241249280b7da
MuYunyun <328375795@qq.com>
2021-05-04 13:48:15 +0800

    docs: polar coord

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-# chart
+# Chart
 
-当前支持绘制折线图、曲线图、面积图。
+当前支持绘制`折线图`、`曲线图`、`面积图`。
 
-| 属性         | 说明             | 类型                       | 默认值   |
-| :----------- | :--------------- | :------------------------- | :------- |
-| id           | 元素节点位置     | string                     |          |
-| width        | 宽度             | number                     |          |
-| height       | 高度             | number                     |          |
-| marginLeft   | 左间距           | number                     | 0        |
-| marginBottom | 下间距           | number                     | 0        |
-| marginTop    | 上间距           | number                     | 0        |
-| xArr         | 水平方向节点坐标 | number[]                   |          |
-| yArr         | 垂直方向节点坐标 | number[]                   |          |
-| valueArr     | 值坐标           | number[]                   |          |
-| shape        | 线条类型         | 'normal' \| 'smooth(光滑)' | 'normal' |
-| mode         | 模式             | 'normal' \| 'fill(填充)'   | 'normal' |
-| geom         | 几何类型         | 'line' \| 'interval'       | 'line'   |
+| 属性         | 说明             | 类型                       | 默认值 |
+| :----------- | :--------------- | :------------------------- | :----- |
+| id           | 元素节点位置     | string                     |        |
+| width        | 宽度             | number                     |        |
+| height       | 高度             | number                     |        |
+| marginLeft   | 左间距           | number                     | 0      |
+| marginBottom | 下间距           | number                     | 0      |
+| marginTop    | 上间距           | number                     | 0      |
+| xArr         | 水平方向节点坐标 | number[]                   |        |
+| yArr         | 垂直方向节点坐标 | number[]                   |        |
+| valueArr     | 值坐标           | number[]                   |        |
+| shape        | 线条类型         | 'normal' \| 'smooth(光滑)' | normal |
+| mode         | 模式             | 'normal' \| 'fill(填充)'   | normal |
+| geom         | 几何类型         | 'line' \| 'interval'       | line   |
+| coord        | 坐标系类型       | 'normal' \| 'polar'        | normal |
 
 ### todoList
 
@@ -23,7 +24,7 @@
 - [x] 支持面积图
 - [x] 支持柱状图
 - [ ] 支持雷达图
-- [ ] 处理矩形数据
+- [ ] 处理巨形数据
 
 ### link
 
--- chart.js
+++ chart.js
@@ -10,7 +10,8 @@ const Chart = function({
   valueArr,
   shape,
   mode,
-  geom = 'line'
+  geom = 'line',
+  coord = 'normal'
 }) {
   const effectWidth = width - marginLeft
   const effectHeight = height - marginBottom - marginTop
@@ -24,105 +25,75 @@ const Chart = function({
 
   ctx.fillStyle = '#fff'
   ctx.fillRect(0, 0, width, height)
-  this.drawCoordinateSystem({
-    ctx,
-    effectWidth,
-    height,
-    marginLeft,
-    marginBottom
-  })
 
-  // 绘制横向坐标轴
-  ctx.save()
-  for (let i = 0; i < 6; i++) {
-    ctx.translate(0, perYLength)
-    ctx.lineWidth = 0.08
-    this.drawLine({ ctx: ctx, x0: 0, y0: 0, x1: width, y1:0 })
-  }
-  ctx.restore()
+  if (coord === 'normal') {
+    this.drawCoordinateSystem({
+      ctx,
+      effectWidth,
+      height,
+      marginLeft,
+      marginBottom
+    })
 
-  // 绘制刻度
-  ctx.save()
-  for (let i = 0; i < xArr.length + 1; i++) {
-    this.drawLine({ ctx: ctx, x0: 0, x1: 0, y0: 0, y1: -5 })
-    ctx.lineWidth = 0.2
-    ctx.translate(perXLength, 0)
-  }
-  ctx.restore()
-
-  // 绘制 x 轴下方文字
-  ctx.save()
-  ctx.scale(1, -1)
-  for (let i = 0; i < xArr.length; i++) {
-    ctx.fillStyle = '#000'
-    if (i === 0) {
-      const textWidth = ctx.measureText(xArr[i])
-      ctx.translate(perXLength / 2 - textWidth.width / 2, 15)
-    } else {
-      ctx.translate(perXLength, 0)
-    }
-    ctx.fillText(xArr[i], 0, 0)
-  }
-  ctx.restore()
-
-  // 绘制 y 轴左侧文字
-  ctx.save()
-  ctx.scale(1, -1)
-  ctx.translate(-20, 0)
-  for (let i = 0; i < xArr.length; i++) {
-    ctx.fillStyle = '#000'
-    ctx.fillText(yArr[i], 0, 0)
-    ctx.translate(0, -perYLength)
-  }
-  ctx.restore()
-
-  if (geom === 'line') {
-    // 绘制折线/曲线
+    // 绘制横向坐标轴
     ctx.save()
-    for (let i = 0; i < xArr.length - 1; i++) {
-      ctx.fillStyle = '#000'
-      const curPointX = (1 / 2 + i) * perXLength
-      const curPointY = valueArr[i]
-      const nextPointX = (1 / 2 + i + 1) * perXLength
-      const nextPointY = valueArr[i + 1]
-
-      if (shape === 'smooth') {
-        const [middleX, middleY] = getMiddlePoint(curPointX, curPointY, nextPointX, nextPointY)
-        const [controlX0, controlY0] = getMiddlePoint(middleX, middleY, curPointX, curPointY)
-        const [controlX1, controlY1] = getMiddlePoint(middleX, middleY, nextPointX, nextPointY)
-        ctx.beginPath()
-        ctx.lineTo(curPointX, curPointY)
-        // this value will be affectd by the coordinate value
-        const offset = 8
-        // ![](http://with.muyunyun.cn/d5afa118ee64691e35d17c1aba8b070e.jpg)
-        if (nextPointY > curPointY) {
-          ctx.bezierCurveTo(controlX0 + offset, controlY0 - offset, controlX1 - offset, controlY1 + offset, nextPointX, nextPointY)
-        } else {
-          ctx.bezierCurveTo(controlX0 + offset, controlY0 + offset, controlX1 - offset, controlY1 - offset, nextPointX, nextPointY)
-        }
-        ctx.stroke()
-        ctx.strokeStyle = '#000'
-      } else {
-        this.drawLine({ ctx: ctx, x0: curPointX, y0: curPointY, x1: nextPointX, y1: nextPointY })
-      }
+    for (let i = 0; i < 6; i++) {
+      ctx.translate(0, perYLength)
+      ctx.lineWidth = 0.08
+      this.drawLine({ ctx: ctx, x0: 0, y0: 0, x1: width, y1:0 })
     }
     ctx.restore()
 
-    // 绘制面积
-    if (mode === 'fill') {
-      ctx.beginPath()
-      ctx.moveTo((1 / 2) * perXLength, 0)
-      ctx.lineTo((1 / 2) * perXLength, valueArr[0])
+    // 绘制刻度
+    ctx.save()
+    for (let i = 0; i < xArr.length + 1; i++) {
+      this.drawLine({ ctx: ctx, x0: 0, x1: 0, y0: 0, y1: -5 })
+      ctx.lineWidth = 0.2
+      ctx.translate(perXLength, 0)
+    }
+    ctx.restore()
 
-      for (let i = 0; i < xArr.length; i++) {
+    // 绘制 x 轴下方文字
+    ctx.save()
+    ctx.scale(1, -1)
+    for (let i = 0; i < xArr.length; i++) {
+      ctx.fillStyle = '#000'
+      if (i === 0) {
+        const textWidth = ctx.measureText(xArr[i])
+        ctx.translate(perXLength / 2 - textWidth.width / 2, 15)
+      } else {
+        ctx.translate(perXLength, 0)
+      }
+      ctx.fillText(xArr[i], 0, 0)
+    }
+    ctx.restore()
+
+    // 绘制 y 轴左侧文字
+    ctx.save()
+    ctx.scale(1, -1)
+    ctx.translate(-20, 0)
+    for (let i = 0; i < xArr.length; i++) {
+      ctx.fillStyle = '#000'
+      ctx.fillText(yArr[i], 0, 0)
+      ctx.translate(0, -perYLength)
+    }
+    ctx.restore()
+
+    if (geom === 'line') {
+      // 绘制折线/曲线
+      ctx.save()
+      for (let i = 0; i < xArr.length - 1; i++) {
+        ctx.fillStyle = '#000'
         const curPointX = (1 / 2 + i) * perXLength
         const curPointY = valueArr[i]
         const nextPointX = (1 / 2 + i + 1) * perXLength
         const nextPointY = valueArr[i + 1]
+
         if (shape === 'smooth') {
           const [middleX, middleY] = getMiddlePoint(curPointX, curPointY, nextPointX, nextPointY)
           const [controlX0, controlY0] = getMiddlePoint(middleX, middleY, curPointX, curPointY)
           const [controlX1, controlY1] = getMiddlePoint(middleX, middleY, nextPointX, nextPointY)
+          ctx.beginPath()
           ctx.lineTo(curPointX, curPointY)
           // this value will be affectd by the coordinate value
           const offset = 8
@@ -132,41 +103,76 @@ const Chart = function({
           } else {
             ctx.bezierCurveTo(controlX0 + offset, controlY0 + offset, controlX1 - offset, controlY1 - offset, nextPointX, nextPointY)
           }
+          ctx.stroke()
+          ctx.strokeStyle = '#000'
         } else {
-          ctx.lineTo(curPointX, curPointY)
+          this.drawLine({ ctx: ctx, x0: curPointX, y0: curPointY, x1: nextPointX, y1: nextPointY })
         }
       }
-      ctx.lineTo((1 / 2 + xArr.length - 1) * perXLength, 0)
-      const gradient = ctx.createLinearGradient(0, 0, 0, 300)
-      gradient.addColorStop(0, 'green');
-      gradient.addColorStop(.5, 'cyan');
-      gradient.addColorStop(1, 'green');
-      // ctx.fillStyle = 'red'
-      ctx.fillStyle = gradient
-      ctx.fill()
-    }
+      ctx.restore()
 
-    // 绘制圆点
-    ctx.save()
-    for (let i = 0; i < xArr.length - 1; i++) {
-      ctx.fillStyle = '#000'
-      const pointX = (1 / 2 + i + 1) * perXLength
-      const pointY = valueArr[i + 1]
-      ctx.beginPath()
-      ctx.arc(pointX, pointY, 2, 0, Math.PI * 2, true)
-      ctx.stroke()
+      // 绘制面积
+      if (mode === 'fill') {
+        ctx.beginPath()
+        ctx.moveTo((1 / 2) * perXLength, 0)
+        ctx.lineTo((1 / 2) * perXLength, valueArr[0])
+
+        for (let i = 0; i < xArr.length; i++) {
+          const curPointX = (1 / 2 + i) * perXLength
+          const curPointY = valueArr[i]
+          const nextPointX = (1 / 2 + i + 1) * perXLength
+          const nextPointY = valueArr[i + 1]
+          if (shape === 'smooth') {
+            const [middleX, middleY] = getMiddlePoint(curPointX, curPointY, nextPointX, nextPointY)
+            const [controlX0, controlY0] = getMiddlePoint(middleX, middleY, curPointX, curPointY)
+            const [controlX1, controlY1] = getMiddlePoint(middleX, middleY, nextPointX, nextPointY)
+            ctx.lineTo(curPointX, curPointY)
+            // this value will be affectd by the coordinate value
+            const offset = 8
+            // ![](http://with.muyunyun.cn/d5afa118ee64691e35d17c1aba8b070e.jpg)
+            if (nextPointY > curPointY) {
+              ctx.bezierCurveTo(controlX0 + offset, controlY0 - offset, controlX1 - offset, controlY1 + offset, nextPointX, nextPointY)
+            } else {
+              ctx.bezierCurveTo(controlX0 + offset, controlY0 + offset, controlX1 - offset, controlY1 - offset, nextPointX, nextPointY)
+            }
+          } else {
+            ctx.lineTo(curPointX, curPointY)
+          }
+        }
+        ctx.lineTo((1 / 2 + xArr.length - 1) * perXLength, 0)
+        const gradient = ctx.createLinearGradient(0, 0, 0, 300)
+        gradient.addColorStop(0, 'green');
+        gradient.addColorStop(.5, 'cyan');
+        gradient.addColorStop(1, 'green');
+        // ctx.fillStyle = 'red'
+        ctx.fillStyle = gradient
+        ctx.fill()
+      }
+
+      // 绘制圆点
+      ctx.save()
+      for (let i = 0; i < xArr.length - 1; i++) {
+        ctx.fillStyle = '#000'
+        const pointX = (1 / 2 + i + 1) * perXLength
+        const pointY = valueArr[i + 1]
+        ctx.beginPath()
+        ctx.arc(pointX, pointY, 2, 0, Math.PI * 2, true)
+        ctx.stroke()
+      }
+      ctx.restore()
+    } else if (geom === 'interval') {
+      ctx.save()
+      const defaultGap = 10
+      for (let i = 0; i < xArr.length; i++) {
+        const curPointX = i * perXLength
+        ctx.fillStyle = '#1790ff'
+        ctx.fillRect(curPointX + defaultGap, 0, perXLength - 2 * defaultGap, valueArr[i])
+        ctx.fill()
+      }
+      ctx.restore()
     }
-    ctx.restore()
-  } else if (geom === 'interval') {
-    ctx.save()
-    const defaultGap = 10
-    for (let i = 0; i < xArr.length; i++) {
-      const curPointX = i * perXLength
-      ctx.fillStyle = '#1790ff'
-      ctx.fillRect(curPointX + defaultGap, 0, perXLength - 2 * defaultGap, valueArr[i])
-      ctx.fill()
-    }
-    ctx.restore()
+  } else if (coord === 'polar') {
+    // todo
   }
 }
 
